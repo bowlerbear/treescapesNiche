58aa41a89d5c0884fe240212eb738e248d3bd50c
Diana Bowler <diana.e.bowler@gmail.com>
2022-06-20 09:59:54 +0100

    update folder paths

--- 08_traitsTrends.R
+++ 08_traitsTrends.R
@@ -1,6 +1,8 @@
 ### species-level trends vs forest traits ####
 
 library(tidyverse)
+library(ggridges)
+theme_set(theme_classic())
 
 selectTaxa <-  c("Ants", "AquaticBugs","Bees","Bryophytes","Carabids","Centipedes","Craneflies",
                  "Dragonflies","E&D","Ephemeroptera","FungusGnats","Gelechiids","Hoverflies",
@@ -11,10 +13,10 @@ selectTaxa <-  c("Ants", "AquaticBugs","Bees","Bryophytes","Carabids","Centipede
 ### choose models #######################
 
 #trends
-trendsFolder <- "outputs/spTrends"
+trendsFolder <- "outputs/speciesTrends"
 
 #forest associations
-forestFolder <- "outputs/HPC/broadleaf/run2"
+forestFolder <- "outputs/forestAssociations/broadleaf"
 
 ### trends ##############################
 
@@ -22,11 +24,26 @@ spTrends <- list.files(trendsFolder, full.names = TRUE) %>%
               set_names() %>%
               map_dfr(readRDS, .id="source") %>%
               group_by(source) %>%
-              mutate(Taxa = strsplit(source,"_")[[1]][2]) %>%
-              mutate(Taxa = gsub(".rds","",Taxa)) %>%
-              ungroup() %>%
+              rename(Taxa = taxon) %>%
               filter(Taxa %in% selectTaxa) 
               
+table(spTrends$Taxa)
+
+spTrends %>%
+  ggplot() +
+  geom_density_ridges(aes(x = mean_change, y = Taxa, fill=Taxa),
+                      rel_min_height = 0.001) +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  theme(legend.position = "none") +
+  xlab("Trends")
+
+#filter those with large uncertainties
+
+hist(spTrends$sd_change)
+summary(spTrends$sd_change)
+
+spTrends <- spTrends %>%
+              filter(sd_change < 3)
 
 ### forest preferences ###################
 
@@ -45,6 +62,11 @@ gamOutputs <- list.files(forestFolder,full.names=TRUE) %>%
 
 df <- inner_join(spTrends, gamOutputs, by = c("species","Taxa"))
 
+### outlier ##############################
+
+df <- df %>%
+        filter(!species %in% c("ara_1391"))
+
 ###plotting ##############################
 
 ggplot(df) +
@@ -55,6 +77,61 @@ ggplot(df) +
 
 ### lms #################################
 
-#for each taxa, fit an lm
+#https://github.com/paul-buerkner/brms/issues/643
+
+getForestEffect <- function(taxa){
+  
+  require(brms)
+  
+  df1 <- subset(df,Taxa==taxa & forest_assoc>0)
+  bm1 <- brm(mean_change|se(sd_change, sigma = TRUE) ~ me(forest_assoc, std_error),
+             data = df1)
+  
+  
+  df2 <- subset(df,Taxa==taxa & forest_assoc<0)
+  bm2 <- brm(mean_change|se(sd_change, sigma = TRUE) ~ me(forest_assoc, std_error),
+             data = df2)
+  
+  
+  data.frame(Taxa = taxa,
+            posEffect = fixef(bm1)[2,1],
+            posEffect_se = fixef(bm1)[2,2],
+            negEffect = fixef(bm2)[2,1],
+            negEffect_se = fixef(bm2)[2,2])
+            
+}
+
+
+lapply(sort(unique(df$Taxa))[26:28], function(x){
+  temp <- getForestEffect(x)
+  saveRDS(temp,file=paste0("outputs/forestVStrends/forestVStrends_",x,".rds"))
+  })
+
+### process lms #########################
+
+lmOutputs <- list.files("outputs/forestVStrends",full.names = TRUE) %>%
+              map_dfr(readRDS)
+
+
+g1 <- ggplot(lmOutputs)+
+  geom_pointrange(aes(x = Taxa, y = posEffect, 
+                      ymin =posEffect - posEffect_se,
+                      ymax = posEffect + posEffect_se),
+                      size = rel(0.3))+
+  geom_hline(yintercept=0, linetype="dashed")+
+  ylab("effect of forest specialisation")+
+  coord_flip()
+
+g2 <- ggplot(lmOutputs)+
+  geom_pointrange(aes(x = Taxa, y = negEffect, 
+                      ymin =negEffect - negEffect_se,
+                      ymax = negEffect + negEffect_se),
+                      size = rel(0.3))+
+  geom_hline(yintercept=0, linetype="dashed")+
+  ylab("effect of forest avoidance")+
+  coord_flip()
+
+cowplot::plot_grid(g1,g2, labels=c("A","B"))
+ggsave("plots/forestVStrends.png",width=6, height=3.5)
 
 ### end ##################################
