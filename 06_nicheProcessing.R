85410ef2b5edc725ba444105daf02120b244b0c4
Diana Bowler <diana.e.bowler@gmail.com>
2022-08-04 11:45:56 +0100

    get linear associations for gamms

--- 06_nicheProcessing.R
+++ 06_nicheProcessing.R
@@ -45,10 +45,10 @@ getModels <- function(modelFolder,modeltype = "simple_linear"){
     fileName <- "gamOutput_gam_shape_subset_random_"
     
   }else if(modeltype=="mixed_linear"){
-    fileName <- "gamOutput_gamm4_subset_random_"
+    fileName <- "gamOutput_gamm_subset_random_"
     
   }else if(modeltype=="mixed_shape"){
-    fileName <- "gamOutput_gamm4_shape_subset_random_"
+    fileName <- "gamOutput_gamm_shape_subset_random_"
   }
   
   list.files(modelFolder,full.names=TRUE) %>%
@@ -71,6 +71,8 @@ getModels <- function(modelFolder,modeltype = "simple_linear"){
 
 gamOutputs <- getModels(modelFolder,modeltype = "simple_linear")
           
+saveRDS(gamOutputs, file="outputs/broadleafAssocations_simple_linear.rds")
+
 #what taxa do we have?
 sort(unique(gamOutputs$Taxa))#all taxa
 
@@ -120,21 +122,60 @@ gamOutputs %>%
   xlab("Conif forest cover %") + ylab("Occupancy")+
   theme_classic()
 
-### linear/gamm4 ##################################################
+### linear/gamm ##################################################
 
 gamOutputs <- getModels(modelFolder,modeltype = "mixed_linear")
 
-#what taxa do we have?
-sort(unique(gamOutputs$Taxa))
-#missing moths
+saveRDS(gamOutputs, file="outputs/forestAssociations/broadleafAssocations_mixed_linear.rds")
 
-### gam shape/gamm4 ###############################################
+#what taxa do we have?
+sort(unique(gamOutputs$Taxa))#all taxa
+
+#order taxa by median preferece
+taxaSummary <- gamOutputs %>%
+  group_by(Taxa) %>%
+  summarise(medEffect = median(estimate)) %>%
+  ungroup() %>%
+  arrange(desc(medEffect))
+gamOutputs$Taxa <- factor(gamOutputs$Taxa, levels=taxaSummary$Taxa)
+
+gamOutputs %>%
+  filter(estimate>(-0.1)) %>%
+  filter(std_error<0.05) %>%
+  ggplot() +
+  geom_density_ridges(aes(x = estimate, y = Taxa, fill=Taxa),
+                      rel_min_height = 0.001) +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  theme(legend.position = "none") +
+  xlab("Association with forest cover")
+
+### gam shape/gamm ###############################################
 
 gamOutputs <- getModels(modelFolder,modeltype = "mixed_shape")
 
 #what taxa do we have?
 sort(unique(gamOutputs$Taxa))
-#missing moths 
+
+speciesMax <- gamOutputs %>%
+  group_by(Species) %>%
+  summarise(maxPred = max(preds)) %>%
+  ungroup()
+
+#for broad leaf
+gamOutputs %>%
+  ggplot()+
+  geom_line(aes(x=decidForest, y=preds, group=Species))+
+  facet_wrap(~Taxa)+
+  xlab("Decid forest cover %") + ylab("Occupancy")+
+  theme_classic()
+
+#for coniferous
+gamOutputs %>%
+  ggplot()+
+  geom_line(aes(x=conifForest, y=preds, group=Species))+
+  facet_wrap(~Taxa)+
+  xlab("Conif forest cover %") + ylab("Occupancy")+
+  theme_classic()
 
 ### compare models #################################################
 
@@ -154,6 +195,7 @@ ggplot(allGams) +
   geom_point(aes(x = estimate_mixed_linear, y = estimate_simple_linear)) +
   facet_wrap(~taxa, scales="free") +
   geom_abline(aes(intercept = 0, slope = 1, linetype = "dashed"))
+#highly correlated
 
 ### compare decid vs conif #######################################
 
@@ -175,35 +217,70 @@ allGams <- bind_rows(gamOutputs_broadleaf,gamOutputs_conif) %>%
 ggplot(allGams) +
   geom_point(aes(x = estimate_broadleaf, y = estimate_conif)) +
   facet_wrap(~taxa, scales="free") +
-  geom_abline(aes(intercept = 0, slope = 1),linetype = "dashed")
+  geom_abline(aes(intercept = 0, slope = 1),linetype = "dashed") +
+  geom_hline(yintercept=0)+
+  geom_vline(xintercept=0)
 
 #mostly correlated
+
+### forest special ##############################################
+
+#run previous section
+
+allGams$estimate_broadleafS <- ifelse(allGams$estimate_broadleaf<0,0.0001,
+                                     allGams$estimate_broadleaf)
+
+allGams$estimate_conifS <- ifelse(allGams$estimate_conif<0,0.0001,
+                                     allGams$estimate_conif)
+
+allGams$ForestSpecial <- log(allGams$estimate_broadleafS/allGams$estimate_conifS)
+
+allGams %>%
+  select(species, taxa, ForestSpecial) %>%
+  saveRDS(., file="outputs/forestAssociations/forestSpecial_simple_linear.rds")
+
+ggplot(allGams) +
+  geom_point(aes(x = estimate_broadleaf, y = estimate_conif, 
+                 colour=ForestSpecial)) +
+  facet_wrap(~taxa, scales="free") +
+  geom_abline(aes(intercept = 0, slope = 1),linetype = "dashed") +
+  scale_color_gradient2()+
+  geom_hline(yintercept=0)+
+  geom_vline(xintercept=0)
+
+ggplot(allGams) +
+  geom_point(aes(x = estimate_broadleaf, y = estimate_conif, 
+                 colour=ForestSpecial)) +
+  geom_abline(aes(intercept = 0, slope = 1),linetype = "dashed") +
+  scale_color_gradient2() +
+  geom_hline(yintercept=0) + geom_vline(xintercept=0) +
+  xlim(-0.1,0.1) + ylim(-0.1,0.1)
 
 ### occ-det models ##############################################
 
 modelFolder <- "outputs/forestAssociations/broadleaf/occuModels"
 
 carabidMods <- list.files(modelFolder,full.names=TRUE) %>%
-                  str_subset("occuGam_forestcover") %>%
-                  set_names() %>%
-                  map_dfr(readRDS, .id="source") %>%
-                  group_by(source) %>%
-                  mutate(species = strsplit(source,"_")[[1]][4]) %>%
-                  mutate(species = gsub(".rds","", species)) %>%
-                  ungroup() %>%
-                  rename(estimate_occdet = estimate) %>%
-                  filter(para == "decidForest")
+  str_subset("occuGam_forestcover") %>%
+  set_names() %>%
+  map_dfr(readRDS, .id="source") %>%
+  group_by(source) %>%
+  mutate(species = strsplit(source,"_")[[1]][4]) %>%
+  mutate(species = gsub(".rds","", species)) %>%
+  ungroup() %>%
+  rename(estimate_occdet = estimate) %>%
+  filter(para == "decidForest")
 
 gamOutputs <- getModels(modelFolder = "outputs/forestAssociations/broadleaf",
                         modeltype = "simple_linear") %>%
-              filter(Taxa == "Carabids") %>%
-                mutate(species = gsub("Col_", "", species))
+  filter(Taxa == "Carabids") %>%
+  mutate(species = gsub("Col_", "", species))
 
 compareOutput <- inner_join(carabidMods, gamOutputs, by = "species")
 
 
 qplot(estimate_occdet, estimate, data=compareOutput)
-#bit different
+#bit different but mostly similar
 cor(compareOutput$estimate_occdet,compareOutput$estimate)
 
 ### end ##########################################################
