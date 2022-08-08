24a0e61ce29fcb1e145e5117153e2ec64672468e
Diana Bowler <diana.e.bowler@gmail.com>
2022-08-08 11:57:09 +0100

    cross-check correlation results

--- 07_clusterAnalysis.R
+++ 07_clusterAnalysis.R
@@ -50,6 +50,7 @@ gamSummary <- gamOutputs %>%
             sdPreds = sd(preds))
 
 #what prop of species show no variation
+
 mean(gamSummary$sdPreds==0) # few!! 10 species
 
 #remove species were the sd is 0 - prob too rare to be modelled
@@ -57,8 +58,10 @@ gamOutputs <- gamOutputs %>%
   filter(Species %in% gamSummary$Species[gamSummary$sdPreds!=0])
 
 #remove species with large sd
+
 quantile(gamSummary$sdPreds, 0.95)
 mean(gamSummary$sdPreds>0.1)
+
 gamOutputs <- gamOutputs %>%
   filter(Species %in% gamSummary$Species[gamSummary$sdPreds<0.1])
 nrow(gamOutputs)
@@ -146,7 +149,7 @@ gamOutputs %>%
   xlab("Decid forest cover %") + ylab("Occupancy")+
   facet_wrap(~cluster)
 
-ggsave("plots/clustering_all_kmeans_means.png")
+ggsave("plots/clustering_all_pam_means.png")
 
 #how each taxa is distributed in each cluster
 gamOutputs %>%
@@ -157,13 +160,13 @@ ggplot() +
   geom_col(aes(x=Taxa, y = nuSpecies, fill = factor(cluster))) +
   coord_flip()
 
-ggsave("plots/clustering_all_kmeans_prop.png")
+ggsave("plots/clustering_all_pam_prop.png")
 
 #save classification
 gamOutputs %>%
   select(Species, cluster) %>%
   filter(!duplicated(Species)) %>%
-  saveRDS(., file="outputs/clustering/kmeans_classification_all.rds")
+  saveRDS(., file="outputs/clustering/pam_classification_all.rds")
 
 #### correlation based ##############################
 
@@ -269,28 +272,33 @@ getDerivatives <- function(myspecies){
 }
 
 allDerivatives <- lapply(sort(unique(gamOutputs$Species)), function(x){
-  print(x)
+  #print(x)
   getDerivatives(x)
 }) %>%
    reduce(rbind)
 
-# derivatives taken directly from fitted model
+saveRDS(allDerivatives, 
+        file="outputs/clustering/allDerivatives.rds")
 
-allDerivatives <- list.files(modelFolder,full.names=TRUE) %>%
-  str_subset("gamm_derivatives") %>%
-  set_names() %>%
-  map_dfr(readRDS, .id="source") %>%
-  group_by(source) %>%
-  mutate(Taxa = strsplit(source, "gamm_derivatives_subset_random_decid_")[[1]][2]) %>%
-  mutate(Taxa = strsplit(Taxa,"_")[[1]][1]) %>%
-  ungroup() %>%
-  filter(Taxa %in% selectTaxa) %>%
-  mutate(Taxa = case_when(Taxa=="E&D" ~ "Empidid",
-                          TRUE ~ as.character(Taxa)))
-#filter extremes?? 
-temp <- subset(allDerivatives, deriv)
+# # derivatives taken directly from fitted model
+# 
+# allDerivatives <- list.files(modelFolder,full.names=TRUE) %>%
+#   str_subset("gamm_derivatives") %>%
+#   set_names() %>%
+#   map_dfr(readRDS, .id="source") %>%
+#   group_by(source) %>%
+#   mutate(Taxa = strsplit(source, "gamm_derivatives_subset_random_decid_")[[1]][2]) %>%
+#   mutate(Taxa = strsplit(Taxa,"_")[[1]][1]) %>%
+#   ungroup() %>%
+#   filter(Taxa %in% selectTaxa) %>%
+#   mutate(Taxa = case_when(Taxa=="E&D" ~ "Empidid",
+#                           TRUE ~ as.character(Taxa)))
+# #filter extremes?? 
+# temp <- subset(allDerivatives, deriv)
 
 #continue here:
+
+allDerivatives <- readRDS("outputs/clustering/allDerivatives.rds")
 
 #pam on them
 mydata <- reshape2::acast(allDerivatives,
@@ -325,6 +333,9 @@ nrow(gamOutputs)#loose 0%
 
 #better clustering!!!
 tapply(gamOutputs$sil_width, gamOutputs$cluster, mean)
+tapply(gamOutputs$sil_width, gamOutputs$cluster, min)
+#1         2         3         4 
+#0.8817067 0.9465818 0.8045520 0.8061120 
 
 #species within
 gamOutputs %>%
@@ -351,10 +362,10 @@ gamOutputs %>%
 
 #mean per cluster
 gamOutputs %>%
-  mutate(cluster = case_when(cluster==1 ~ 'flat',
-                             cluster==2 ~ 'humped',
-                             cluster==3 ~ 'open',
-                             cluster==4 ~ 'forest')) %>%
+  mutate(cluster = case_when(cluster==1 ~ 'open',
+                             cluster==2 ~ 'flat',
+                             cluster==3 ~ 'forest',
+                             cluster==4 ~ 'humped')) %>%
   group_by(cluster,decidForest) %>%
   summarise(preds = median(preds)) %>%
   ggplot()+
@@ -373,10 +384,10 @@ gamOutputs %>%
   group_by(Taxa, cluster) %>%
   summarise(nuSpecies = length(unique(Species))) %>%
   ungroup() %>%
-  mutate(cluster = case_when(cluster==1 ~ 'flat',
-                             cluster==2 ~ 'humped',
-                             cluster==3 ~ 'open',
-                             cluster==4 ~ 'forest')) %>%
+  mutate(cluster = case_when(cluster==1 ~ 'open',
+                             cluster==2 ~ 'flat',
+                             cluster==3 ~ 'forest',
+                             cluster==4 ~ 'humped')) %>%
   mutate(cluster = factor(cluster, 
                           levels=c("forest","humped","flat","open"))) %>%
   ggplot() +
@@ -384,7 +395,6 @@ gamOutputs %>%
   scale_fill_brewer(palette = "RdYlGn", direction =-1) +
   coord_flip()+
   theme_classic()
-
 ggsave("plots/clustering_all_deriv_prop.png")
 
 gamOutputs %>%
@@ -395,13 +405,92 @@ saveRDS(., file="outputs/clustering/deriv_classification_all.rds")
 ### concordance #####################################
 
 corr_clustering <- readRDS("outputs/clustering/corr_classification_all.rds")
-kmeans_clustering <- readRDS("outputs/clustering/kmeans_classification_all.rds")
+deriv_clustering <- readRDS("outputs/clustering/deriv_classification_all.rds")
 
-all_clustering <- inner_join(corr_clustering,kmeans_clustering, by="Species")
+all_clustering <- inner_join(corr_clustering, deriv_clustering, by="Species")
 
 table(all_clustering$cluster.x, all_clustering$cluster.y)
 
-#pretty good!!
+#plots of the differences
+
+#corr - 1 is mostly flat, 
+all_clustering %>%
+  filter(cluster.x=="1") %>%
+  left_join(gamOutputs) %>%
+  ggplot()+
+  geom_line(aes(x = decidForest, y = preds, groups = Species))+
+  xlab("Decid forest cover %") + ylab("Occupancy")+
+  facet_wrap(~cluster.y)
+#many are put in the flat category based on the derivs
+
+#corr - 2 is forest, 
+all_clustering %>%
+        filter(cluster.x=="2") %>%
+        left_join(gamOutputs) %>%
+  ggplot()+
+  geom_line(aes(x = decidForest, y = preds, groups = Species))+
+  xlab("Decid forest cover %") + ylab("Occupancy")+
+  facet_wrap(~cluster.y)
+#many are put in the flat category based on the derivs 
+#but they seem to only increase slightly with forest, so seems ok
+
+#corr - 4 is humped
+all_clustering %>%
+  filter(cluster.x=="4") %>%
+  left_join(gamOutputs) %>%
+  ggplot()+
+  geom_line(aes(x = decidForest, y = preds, groups = Species))+
+  xlab("Decid forest cover %") + ylab("Occupancy")+
+  facet_wrap(~cluster.y)
+#most also humped according to the derivatives
+#few are put into the forest cover based on derivs 
+#but these are only slighty humped
+
+#deriv - 2 is flat
+all_clustering %>%
+  filter(cluster.y=="2") %>%
+  left_join(gamOutputs) %>%
+  ggplot()+
+  geom_line(aes(x = decidForest, y = preds, groups = Species))+
+  xlab("Decid forest cover %") + ylab("Occupancy")+
+  facet_wrap(~cluster.x)
+#mostly flat
+
+#deriv - 3 is forest
+all_clustering %>%
+  filter(cluster.y=="3") %>%
+  left_join(gamOutputs) %>%
+  ggplot()+
+  geom_line(aes(x = decidForest, y = preds, groups = Species))+
+  xlab("Decid forest cover %") + ylab("Occupancy")+
+  facet_wrap(~cluster.x)
+
+# a few are U-shaped??
+outliers <- all_clustering$Species[all_clustering$cluster.y==3 &
+                         all_clustering$cluster.x==1]
+
+#check pattern without these
+all_clustering %>%
+  filter(cluster.y=="3") %>%
+  left_join(gamOutputs) %>%
+  ggplot()+
+  geom_line(aes(x = decidForest, y = preds, groups = Species))+
+  xlab("Decid forest cover %") + ylab("Occupancy")+
+  facet_wrap(~Taxa)
+
+deriv_clustering %>%
+  filter(!Species %in% outliers) %>%
+saveRDS(.,"outputs/clustering/deriv_classification_all.rds")
+
+#deriv - 4 is humped
+all_clustering %>%
+  filter(cluster.y=="4") %>%
+  left_join(gamOutputs) %>%
+  ggplot()+
+  geom_line(aes(x = decidForest, y = preds, groups = Species))+
+  xlab("Decid forest cover %") + ylab("Occupancy")+
+  facet_wrap(~cluster.x)
+#humped seems reasonable
 
 ### end #############################################
 
