f26657eff5870bb8f4f374b7af3105b883c5523e
Diana Bowler <diana.e.bowler@gmail.com>
2022-08-09 15:25:28 +0100

    comparing forest association against habitat classification

--- 10_databasetraits.R
+++ 10_databasetraits.R
@@ -6,7 +6,7 @@ selectTaxa <-  c("Ants", "AquaticBugs","Bees","Carabids","Centipedes","Craneflie
                  "Dragonflies","E&D","Ephemeroptera","Gelechiids","Hoverflies",
                  "Ladybirds","LeafSeedBeetles","Molluscs","Moths",
                  "Orthoptera","PlantBugs","ShieldBugs",
-                 "Soldierflies","Spiders","Trichoptera","Wasps","Weevils")
+                 "Soldierflies","Spiders","Trichoptera","Wasps","Weevil")
 
 ### trends ##############################
 
@@ -41,6 +41,11 @@ spTrends <- spTrends %>%
 speciesNames <- read.csv("C:/Users/diabow/OneDrive - UKCEH/Projects/General/masterLookup.csv", as.is=TRUE)
 speciesNames$CONCEPT[is.na(speciesNames$CONCEPT)] <- speciesNames$NAME[is.na(speciesNames$CONCEPT)]
 speciesNames$CONCEPT <- tolower(speciesNames$CONCEPT)
+
+#get rid of initials
+speciesNames$NAME <- gsub(" s.l.| agg| s.s.| s.str.| agg.| B","", speciesNames$NAME)
+
+#add to trends file
 spTrends$Species <- speciesNames$NAME[match(spTrends$species, speciesNames$CONCEPT)]
 
 #our all out species in this file
@@ -63,30 +68,32 @@ indexFile <- read_csv(paste(indexDir,"species-index.csv",sep="/")) %>%
   rename(Species = species)
 
 #what species are missing from this database
-mean(unique(spTrends$Species) %in% indexFile$Species) # #75% of species
+mean(unique(spTrends$Species) %in% indexFile$Species) # #77% of species
 taxaData <- unique(spTrends[,c("Species","Taxa")])
 taxaData$Present <- taxaData$Species %in% indexFile$Species 
 tapply(taxaData$Present,taxaData$Taxa,mean)
 
 #can we get any of these as a synonymn
-missingSpecies <- sort(unique(spTrends$Species))[!unique(spTrends$Species) %in%
-                                                   indexFile$Species]
+#missingSpecies <- sort(unique(spTrends$Species))[!unique(spTrends$Species) %in%
+#                                                   indexFile$Species]
 
 #join all
 spTrends <- spTrends %>%
   left_join(.,indexFile, by="Species") 
 
-### habitat traits #####
+### habitat vs trends #####
 
 names(indexFile)
 table(spTrends$broad_biotope)
 
+#how much data is there?
+mean(!is.na(spTrends$broad_biotope))
+
 #broad classification
 treeDF <- spTrends %>%
-          filter(broad_biotope == "tree-associated")
+          filter(grepl("tree-associated", broad_biotope))
 
 g1 <- treeDF %>%
-  filter(!Taxa %in% c("Trichoptera","Orthoptera")) %>% # little data for these
 ggplot()+
   geom_boxplot(aes(x=Taxa, y = median_change)) +
   ylab("Species growth rate")+
@@ -96,14 +103,17 @@ ggplot()+
 #broad classification
 table(spTrends$habitat)
 
+#how much data is there?
+mean(!is.na(spTrends$habitat))
+
 treeDF <- spTrends %>%
   filter(habitat %in% c("arboreal","decaying wood","shaded woodland floor"))
 
 g2 <- treeDF %>%
-  filter(!Taxa %in% c("Trichoptera","Orthoptera","Ladybirds")) %>% # little data for these
   ggplot()+
-  geom_boxplot(aes(x=Taxa, y = median_change, fill=habitat)) +
+  geom_boxplot(aes(x=Taxa, y = median_change)) +
   ylab("Species growth rate")+
+  facet_wrap(~habitat) +
   geom_hline(yintercept=0, linetype="dashed")+
   coord_flip() 
 
@@ -111,6 +121,31 @@ cowplot::plot_grid(g1,g2,
                    nrow=2)
 
 ggsave("plots/pantheon_traits.png",width=5.5,height=6)
+
+### habitat vs new metric ####
+
+gamOutputs <- readRDS("outputs/forestAssociations/broadleafAssocations_mixed_linear.rds") %>%
+                  mutate(species = tolower(species))
+
+#sort species names
+gamOutputs$Species <- speciesNames$NAME[match(gamOutputs$species, speciesNames$CONCEPT)]
+gamOutputs$Species[is.na(gamOutputs$Species)] <- gamOutputs$species[is.na(gamOutputs$Species)]
+gamOutputs$Species <- tolower(gamOutputs$Species)
+
+#merge with habitat data
+gamOutputs <- indexFile %>%
+                mutate(Species = tolower(Species)) %>%
+                inner_join(.,gamOutputs, by="Species")
+               
+gamOutputs$habitatS <- ifelse(grepl("tree-associated", gamOutputs$broad_biotope),
+                              "Tree","Other")
+ 
+gamOutputs %>%
+  filter(abs(estimate)<0.05) %>%
+ggplot() +
+  geom_boxplot(aes(x=Taxa, y=estimate, fill=habitatS)) +
+  geom_hline(yintercept=0, linetype="dashed") +
+  coord_flip()
 
 ### conservation status ####
 
