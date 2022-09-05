def9333e34469ea393ea252e1d45efa3c6bfe13b
Diana Bowler <diana.e.bowler@gmail.com>
2022-09-05 14:30:25 +0100

    move getModels function here

--- 00_functions.R
+++ 00_functions.R
@@ -24,3 +24,48 @@ makeCapital <- function(string){
   })
   
 }
+
+getModels <- function(modelFolder,modeltype = "simple_linear"){ 
+  
+  #translate
+  if(modeltype=="simple_linear"){
+    fileName <- "gamOutput_glm_subset_random_"
+    
+  } else if(modeltype=="simple_shape"){
+    fileName <- "gamOutput_gam_shape_subset_random_"
+    
+  }else if(modeltype=="mixed_linear"){
+    fileName <- "gamOutput_gamm_subset_random_"
+    
+  }else if(modeltype=="mixed_shape"){
+    fileName <- "gamOutput_gamm_shape_subset_random_"
+  }
+  
+  temp <- list.files(modelFolder,full.names=TRUE) %>%
+    str_subset(fileName) %>%
+    set_names() %>%
+    map_dfr(readRDS, .id="source") %>%
+    group_by(source) %>%
+    mutate(Taxa = strsplit(source, fileName)[[1]][2]) %>%
+    mutate(Taxa = gsub("conif_","",Taxa)) %>%
+    mutate(Taxa = gsub("decid_","",Taxa)) %>%
+    mutate(Taxa = strsplit(Taxa,"_")[[1]][1])%>%
+    ungroup() %>%
+    filter(Taxa %in% selectTaxa) %>%
+    mutate(Taxa = case_when(Taxa=="E&D" ~ "Empidids",
+                            TRUE ~ as.character(Taxa)))  %>%
+    add_column(modeltype = modeltype)
+  
+  #fix names
+  
+  if("Species" %in% names(temp)){
+    temp <- temp %>% rename(Species = species)
+  }
+  
+  if("decid_forest" %in% names(temp)){
+    temp <- temp %>% rename(decidForest = decid_forest)
+    
+  }
+  
+  return(temp)
+}
